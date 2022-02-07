b5658a03d573f65b4af8fc2f6f53ebb2e1dae73f
lujet <462546997@qq.com>
2022-02-08 00:11:46 +0800

    对照原版书籍进行校正, 添加理解

--- file/toupper.s
+++ file/toupper.s
@@ -9,13 +9,33 @@
 #       - 读取内存缓冲区的每个字节, 如果该字母为小写字母, 就将其转换为大写字母
 #       - 将内存缓冲区写入输出文件
 #
+# 缓冲区:
+#	 缓冲区是连续的字节块, 用于批量数据传输; 当要读取文件时, 操作系统需要有一个
+#	 地方来存储读取的数据, 这个地方称为缓冲区; 一般缓冲区仅用于暂时存储数据, 
+#	 然后数据被从缓冲区中读出并转换成便于程序处理的形式;
+# .bss:
+#    .bss 段类似与数据段(.data), 不同的是它不占用可执行程序空间, .bss 段可以
+#    保留存储位置, 却不能对其初始化; 在数据段中, 既可以保留存储位置, 也能为其
+#	 设置初始值(TODO: 原理)
+#
+# 
+# 文件: (TODO: unix 文件处理, 系统调用源码)
+#    无论 unix 文件是什么程序创建的, 都可以作为连续的字节流进行访问; 当访问一个
+#	 文件时, 通过文件名打开它, 操作系统会提供一个编号, 称为文件描述符, 用文件
+#	 描述符指代文件, 直到使用完毕; 可以使用文件描述符对文件进行读取和写入, 完成
+#    读取和写入后, 关闭文件, 关闭后文件描述符即失效.
+#  
+#    基于 unix 的操作系统把所有输入/输出系统都视作文件, 网络连接视为文件,
+#	 串行端口视为文件, 甚至音频设备都被视为文件; 进程之间的通信是通过称为
+#    管道的特殊文件实现的(TODO)
+
 
 .section .data
 
 
 ####### 常数 #######
 
-# 系统调用号
+# 系统调用号 TODO: 熟练运用常用的系统调用(golang语言的相关调用源码)
 .equ SYS_OPEN, 5
 .equ SYS_WRITE, 4
 .equ SYS_READ, 3
@@ -24,7 +44,7 @@
 
 # 文件打开选项(不同的值参考: /usr/include/asm-generic/fcntl.h)
 .equ O_RDONLY, 0
-.equ O_CREAT_WRONLY_TRUNC, 03101 # (TODO:?)
+.equ O_CREAT_WRONLY_TRUNC, 03101 # (TODO:详细)
 
 # 标准文件描述符
 .equ STDIN, 0
@@ -49,14 +69,17 @@
 
 
 # 栈位置(TODO)
-# TODO: 如何获取命令行参数
 .equ ST_SIZE_RESERVE, 8
-.equ ST_FD_IN, -4
-.equ ST_FD_OUT, -8
-.equ ST_ARGC, 0 # 参数数目 
-.equ ST_ARGV_0, 4 # 程序名
-.equ ST_ARGV_1, 8 # 输入文件名
-.equ ST_ARGV_2, 12 # 输出文件名
+.equ ST_FD_IN, 0
+.equ ST_FD_OUT, 4
+.equ ST_ARGC, 8 # 参数数目 
+.equ ST_ARGV_0, 12 # 程序名
+.equ ST_ARGV_1, 16 # 输入文件名
+.equ ST_ARGV_2, 20 # 输出文件名
+# linux 命令行参数存放在一个易于访问的位置, 而且以空字符串结束(TODO: 何意); 
+# 当 Linux 程序开始时, 所有指向命令行参数的指针都存储于栈中; 
+# 参数数目存储于 8(%esp), 程序名存储于 12(%esp), 而参数存储于 16(%esp) 及
+# 其后的存储位置
 
 
 
@@ -64,10 +87,9 @@
 _start:
 	###  程序初始化 ###
 	# 保存栈指针
-	movl %esp, %ebp
-	
 	# 在栈上为文件描述符分配空间
 	subl $ST_SIZE_RESERVE, %esp
+	movl %esp, %ebp
 	
 	
 
@@ -78,13 +100,10 @@ open_fd_in:
 	movl $SYS_OPEN, %eax
     # 将输入文件名放如 %ebx
 	movl ST_ARGV_1(%ebp), %ebx
-	
 	# 只读标志
 	movl $O_RDONLY, %ecx
-	
 	# 权限
 	movl $0666, %edx
-	
 	# 调用 linux
 	int $LINUX_SYSCALL
 	
@@ -140,7 +159,7 @@ store_fd_out:
 		pushl %eax # 缓冲区大小
 		call convert_to_upper
 		popl %eax   # 重新获取大小
-		addl $4, %esp # 恢复 %esp
+		popl %ebx
 
 		### 将字符块写入输出文件 ###
 		# 缓冲区大小
